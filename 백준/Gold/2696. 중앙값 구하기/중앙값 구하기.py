c820383eaf1555b856f66f63bde05f3982f65fe1
박효종 <phj2784@naver.com>
2022-10-04 09:04:37 +0900

    [Gold II] Title: 중앙값 구하기, Time: 84 ms, Memory: 32908 KB -BaekjoonHub

---
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -0,0 +1,47 @@
+import heapq
+import sys
+
+
+def solution():
+    global numbers
+    result = [numbers[0]]
+    mid_value = numbers[0]
+    high = []
+    low = []
+
+    for i, value in enumerate(numbers[1:], 1):
+        if value > mid_value:
+            heapq.heappush(high, value)
+        else:
+            heapq.heappush(low, -value)
+
+        if i % 2 == 0:
+            if len(low) > len(high):
+                heapq.heappush(high, mid_value)
+                mid_value = -heapq.heappop(low)
+            elif len(low) < len(high):
+                heapq.heappush(low, -mid_value)
+                mid_value = heapq.heappop(high)
+            result.append(mid_value)
+
+    print(len(result))
+    for i in range(len(result)):
+        if i != 0 and i % 10 == 0:
+            print()
+        print(result[i], end=' ')
+    print()
+
+
+input = sys.stdin.readline
+T = int(input().rstrip())
+
+for _ in range(T):
+    N = int(input())
+    numbers = []
+    if N % 10 == 0:
+        for i in range(N // 10):
+            numbers.extend(list(map(int, input().rstrip().split())))
+    else:
+        for i in range(N // 10 + 1):
+            numbers.extend(list(map(int, input().rstrip().split())))
+    solution()
