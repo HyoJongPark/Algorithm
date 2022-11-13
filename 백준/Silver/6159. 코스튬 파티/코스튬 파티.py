2a2dca3dcb0098cdce73f6888229ea0f71d73924
박효종 <phj2784@naver.com>
2022-11-13 17:35:04 +0900

    [Silver V] Title: 코스튬 파티, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/6159. 코스튬 파티/코스튬 파티.py
+++ 백준/Silver/6159. 코스튬 파티/코스튬 파티.py
@@ -7,11 +7,11 @@ cows.sort()
 
 left, right = 0, N - 1
 answer = 0
-for left in range(N - 1):
-    for right in range(N - 1, left, -1):
-        sum_of_weight = cows[left] + cows[right]
-        if sum_of_weight <= S:
-            answer += right - left
-            break
-
+while left < right:
+    sum_of_weight = cows[left] + cows[right]
+    if sum_of_weight <= S:
+        answer += right - left
+        left += 1
+    else:
+        right -= 1
 print(answer)
