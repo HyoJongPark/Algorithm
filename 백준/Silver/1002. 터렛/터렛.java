28055702b3579a30ed48241981c722e8e0c24f85
박효종 <phj2784@naver.com>
2022-12-26 20:09:38 +0900

    [Silver III] Title: 터렛, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int r1 = Integer.parseInt(st.nextToken());
+
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+
+            sb.append(calculateEnemyPoint(x1, y1, r1, x2, y2, r2)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static int calculateEnemyPoint(int x1, int y1, int r1, int x2, int y2, int r2) {
+        int distanceOfPoints = (int) (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        if (x1 == x2 && y1 == y2 && r1 == r2) {
+            return -1;
+        } else if (distanceOfPoints > Math.pow(r1 + r2, 2)) {
+            return 0;
+        } else if (distanceOfPoints < Math.pow(r2 - r1, 2)) {
+            return 0;
+        } else if (distanceOfPoints == Math.pow(r2 - r1, 2)) {
+            return 1;
+        } else if (distanceOfPoints == Math.pow(r1 + r2, 2)) {
+            return 1;
+        } else {
+            return 2;
+        }
+
+    }
+
+}
