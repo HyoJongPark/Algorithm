90d5e7fb544883bd95345b8aa2e894de6858d11d
박효종 <phj2784@naver.com>
2023-01-11 16:29:59 +0900

    [Gold V] Title: 치킨 배달, Time: 256 ms, Memory: 18448 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,11 +1,64 @@
-    import java.util.ArrayList;
-    import java.util.List;
-    import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-    import static java.lang.Math.abs;
-    import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
 
-    class Point {
+class Main {
+    static int N, M;
+    static int answer = Integer.MAX_VALUE;
+    static int[] selectedChickenHouse;
+    static List<Point> chicken = new ArrayList<>();
+    static List<Point> house = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                if (tmp == 1) {
+                    house.add(new Point(i, j));
+                } else if (tmp == 2) {
+                    chicken.add(new Point(i, j));
+                }
+            }
+        }
+
+        selectedChickenHouse = new int[M];
+        DFS(0, 0);
+        System.out.println(answer);
+    }
+
+    private static void DFS(int level, int crr) {
+        if (level == M) {
+            int distance = 0;
+            for (Point h : house) {
+                int tmp = Integer.MAX_VALUE;
+                for (int chickenHouseNumber : selectedChickenHouse) {
+                    tmp = Integer.min(tmp, chicken.get(chickenHouseNumber).calculateDistance(h.x, h.y));
+                }
+                distance += tmp;
+            }
+            answer = min(answer, distance);
+            return;
+        }
+
+        for (int chickenHouseNumber = crr; chickenHouseNumber < chicken.size(); chickenHouseNumber++) {
+            selectedChickenHouse[level] = chickenHouseNumber;
+            DFS(level + 1, chickenHouseNumber + 1);
+        }
+    }
+
+    static class Point {
         int x;
         int y;
 
@@ -13,52 +66,9 @@
             this.x = x;
             this.y = y;
         }
-    }
 
-    class Main {
-        static int N;
-        static int M;
-        static int answer = Integer.MAX_VALUE;
-        static List<Point> chicken = new ArrayList<>();
-        static List<Point> house = new ArrayList<>();
-
-        private static void DFS(int level, int[] select, int crr) {
-            if (level == M) {
-                int tmp = 0;
-                for (Point h : house) {
-                    int distance = Integer.MAX_VALUE;
-                    for (int i : select) {
-                        Point c = chicken.get(i);
-                        distance = Integer.min(distance, abs(h.x - c.x) + abs(h.y - c.y));
-                    }
-                    tmp += distance;
-                }
-                answer = min(tmp, answer);
-            } else {
-                for (int i = crr; i < chicken.size(); i++) {
-                    select[level] = i;
-                    DFS(level + 1, select, i+1);
-                }
-            }
-        }
-
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            N = sc.nextInt();
-            M = sc.nextInt();
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    int tmp = sc.nextInt();
-                    if (tmp == 1) {
-                        house.add(new Point(i, j));
-                    } else if (tmp == 2) {
-                        chicken.add(new Point(i, j));
-                    }
-                }
-            }
-
-            DFS(0, new int[M], 0);
-            System.out.println(answer);
+        public int calculateDistance(int x, int y) {
+            return abs(x - this.x) + abs(y - this.y);
         }
     }
+}
