96bdd37ab77e9243dade8110751c2cc57211860b
박효종 <phj2784@naver.com>
2023-02-15 16:10:49 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 136 ms, Memory: 18168 KB -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
+        }
+        System.out.println(dp[N]);
+    }
+}
