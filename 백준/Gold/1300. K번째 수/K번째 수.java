a97dac797b22707e3ccbbfeb97ed51989bf29fe7
박효종 <phj2784@naver.com>
2023-04-09 11:01:07 +0900

    [Gold II] Title: K번째 수, Time: 188 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        long left = 1, right = K;
+        while (left < right) {
+            long mid = (left + right) / 2; //target
+            long count = 0;
+
+            for (int i = 1; i <= N; i++) {
+                if (mid / i == 0) break;
+                count += Math.min(mid / i, N);
+            }
+
+            if (K <= count) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(left);
+    }
+}
