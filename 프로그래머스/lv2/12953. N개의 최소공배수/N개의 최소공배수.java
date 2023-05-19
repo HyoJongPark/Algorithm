b2f4ee7410168dafa96607da181caa12ecb8aaff
박효종 <phj2784@naver.com>
2023-05-19 21:04:16 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr) {
+        if(arr.length == 1) {
+            return arr[0];
+        }
+        
+        int g = gcd(arr[0], arr[1]); 
+        int answer = (arr[0] * arr[1]) / g; 
+        for(int i = 2; i < arr.length; i++) {
+            g = gcd(answer, arr[i]);
+            answer = (answer * arr[i]) / g;
+        }
+        return answer;
+    }
+
+    private static int gcd(int a, int b) {
+        int r;
+        if((r = a % b) == 0) {
+            return b;
+        } else {
+            return gcd(b, r);
+        }
+    }
+}
