384d74f92e50662d03046ecf6f9c94b7e1c6d81a
박효종 <phj2784@naver.com>
2023-08-14 12:54:22 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 716 ms, Memory: 27336 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, K;
+    static int[][] board;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) - 1;
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = Integer.parseInt(st.nextToken()) - 1;
+
+            calculatePrefixSum(x1, y1, x2, y2);
+        }
+        System.out.print(sb);
+    }
+
+    private static void calculatePrefixSum(int x1, int y1, int x2, int y2) {
+        int sum = 0;
+
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                sum += board[i][j];
+            }
+        }
+        sb.append(sum).append("\n");
+    }
+}
