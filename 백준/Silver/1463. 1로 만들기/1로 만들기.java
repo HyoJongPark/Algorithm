ce1c1dbc3690cc71232b5c285fb95de7a0929d90
박효종 <phj2784@naver.com>
2023-10-06 20:15:28 +0900

    [Silver III] Title: 1로 만들기, Time: 204 ms, Memory: 15500 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,44 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class Main {
-
-    static int[] check;
-
+class Main {
+    static int N;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        check = new int[N + 1];
-        Arrays.fill(check, Integer.MAX_VALUE);
+        N = Integer.parseInt(br.readLine());
 
-        DFS(0, N);
-        System.out.println(check[1]);
+        System.out.println(DFS(0, N));
     }
 
-    private static void DFS(int count, int currentVal) {
-        if (currentVal == 1) {
-            check[currentVal] = count;
-            return;
+    private static int DFS(int level, int current) {
+        if (current == 1) return level;
+        else if (current < 1) return Integer.MAX_VALUE;
+        else if (level > min) return Integer.MAX_VALUE;
+
+        int tmp;
+        if (current % 2 == 0) {
+            tmp =  Math.min(
+                    DFS(level + 1, current / 2),
+                    DFS(level + 1, current - 1)
+            );
+
+            min = Math.min(tmp, min);
         }
 
-        if (isValid(3, currentVal, count + 1)) {
-            check[currentVal / 3] = count + 1;
-            DFS(count + 1, currentVal / 3);
-        }
-        if (isValid(2, currentVal, count + 1)) {
-            check[currentVal / 2] = count + 1;
-            DFS(count + 1, currentVal / 2);
-        }
-        if (check[currentVal - 1] > count + 1) {
-            check[currentVal - 1] = count + 1;
-            DFS(count + 1, currentVal - 1);
-        }
-    }
+        if (current % 3 == 0) {
+            tmp =  Math.min(
+                    DFS(level + 1, current / 3),
+                    DFS(level + 1, current - 1)
+            );
 
-    private static boolean isValid(int n, int val, int count) {
-        return val % n == 0 && check[val / n] > count;
+            min = Math.min(tmp, min);
+        }
+
+        if (current % 3 != 0 && current % 2 != 0) {
+            min = Math.min(
+                    DFS(level + 1, current - 1),
+                    min
+            );
+        }
+
+        return min;
     }
 }
