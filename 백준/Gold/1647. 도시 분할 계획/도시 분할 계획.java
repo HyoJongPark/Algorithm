e951b02060882ada3bb1db55abb23d3656385ec0
박효종 <phj2784@naver.com>
2023-12-21 09:54:32 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1228 ms, Memory: 321000 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N, M;
+    static int[] parent;
+    static PriorityQueue<Node> nodes = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        parent = new int[N + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            nodes.offer(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        System.out.println(kruskal());
+    }
+
+    private static int kruskal() {
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        int count = 0, cost = 0;
+        while (!nodes.isEmpty()) {
+            Node current = nodes.poll();
+
+            if (union(current.st, current.en)) {
+                if (count == N - 2) {
+                    break;
+                }
+                
+                cost += current.cost;
+                count++;
+            }
+        }
+        return cost;
+    }
+
+    private static boolean union(int st, int en) {
+        st = find(st);
+        en = find(en);
+
+        if (st == en) {
+            return false;
+        }
+        parent[st] = en;
+        return true;
+    }
+
+    private static int find(int nodeNo) {
+        if (parent[nodeNo] == nodeNo) {
+            return nodeNo;
+        }
+        parent[nodeNo] = find(parent[nodeNo]);
+        return parent[nodeNo];
+    }
+
+    static class Node implements Comparable<Node> {
+        int st, en;
+        int cost;
+
+        public Node(int st, int en, int cost) {
+            this.st = st;
+            this.en = en;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+}
