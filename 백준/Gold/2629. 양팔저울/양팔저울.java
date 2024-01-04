50c2244122ba4bb4728403f08c3d395f041555c0
박효종 <phj2784@naver.com>
2024-01-04 17:50:28 +0900

    [Gold III] Title: 양팔저울, Time: 132 ms, Memory: 15496 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -2,25 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 class Main {
     static int N, M;
-    static int[] weight, target;
+    static int[] weight;
     static boolean[][] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         weight = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        result =  new boolean[N + 1][40_001];
         M = Integer.parseInt(br.readLine());
-        target = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        result = new boolean[N + 1][40001];
 
         dp(0, 0);
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int t : target) {
-            if (result[N][t]) {
+        for (int i = 0; i < M; i++) {
+            if (result[N][Integer.parseInt(st.nextToken())]) {
                 sb.append("Y ");
             } else {
                 sb.append("N ");
@@ -29,14 +30,16 @@ class Main {
         System.out.println(sb);
     }
 
-    private static void dp(int count, int num) {
-        if (result[count][num]) return;
+    private static void dp(int level, int current) {
+        if (result[level][current]) {
+            return;
+        }
 
-        result[count][num] = true;
-        if (count == N) return;
-
-        dp(count + 1, num);
-        dp(count + 1, num + weight[count]);
-        dp(count + 1, Math.abs(num - weight[count]));
+        result[level][current] = true;
+        if (level < N) {
+            dp(level + 1, current);
+            dp(level + 1, current + weight[level]);
+            dp(level + 1, Math.abs(current - weight[level]));
+        }
     }
 }
