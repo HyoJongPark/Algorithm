3fbfeb0dd531fa46b4fc1f2f23865e6b92456732
박효종 <phj2784@naver.com>
2024-01-09 08:57:59 +0900

    [Gold IV] Title: 세부, Time: 884 ms, Memory: 117432 KB -BaekjoonHub

---
+++ 백준/Gold/13905. 세부/세부.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+//크루스칼
+class Main {
+    static int N, M, start, end;
+    static int[] parent;
+    static PriorityQueue<Node> nodes = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+        parent = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            nodes.offer(new Node(a, b, cost));
+        }
+
+        System.out.println(kruskal());
+    }
+
+    private static int kruskal() {
+        int count = 0;
+        while (!nodes.isEmpty()) {
+            Node current = nodes.poll();
+
+            if (union(current.st, current.en)) {
+                if (find(start) == find(end)) {
+                    return current.cost;
+                }
+
+                if (++count == N - 1) {
+                    return 0;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static boolean union(int st, int en) {
+        st = find(st);
+        en = find(en);
+
+        if (st == en) {
+            return false;
+        }
+        parent[st] = en;
+        return true;
+    }
+
+    private static int find(int nodeNo) {
+        if (parent[nodeNo] == nodeNo) {
+            return nodeNo;
+        }
+
+        parent[nodeNo] = find(parent[nodeNo]);
+        return parent[nodeNo];
+    }
+
+    static class Node implements Comparable<Node> {
+        int st, en;
+        int cost;
+
+        public Node(int st, int en, int cost) {
+            this.st = st;
+            this.en = en;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return o.cost - this.cost;
+        }
+    }
+}
