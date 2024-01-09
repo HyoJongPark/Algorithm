730de17b1caedf58f7f3c3d80d88d1c06eaf8f41
박효종 <phj2784@naver.com>
2024-01-09 09:20:53 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 672 ms, Memory: 86276 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
@@ -1,49 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
     static int N;
-    static boolean[] check;
-    static List<Integer>[] nodes;
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        check = new boolean[N + 1];
-        nodes = new List[N + 1];
+        parent = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            nodes[i] = new ArrayList<>();
+            parent[i] = i;
         }
         for (int i = 0; i < N - 2; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            nodes[a].add(b);
-            nodes[b].add(a);
+            union(a, b);
         }
 
-        check[0] = true;
-        check[1] = true;
-        DFS(1);
-
+        int target = find(1);
         for (int i = 2; i <= N; i++) {
-            if (!check[i]) {
+            int current = find(i);
+
+            if (target != current) {
                 System.out.printf("%d %d", 1, i);
                 System.exit(0);
             }
         }
     }
 
-    private static void DFS(int curr) {
-        for (Integer next : nodes[curr]) {
-            if (!check[next]) {
-                check[next] = true;
-                DFS(next);
-            }
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            parent[a] = b;
         }
+    }
+
+    private static int find(int nodeNo) {
+        if (parent[nodeNo] == nodeNo) {
+            return nodeNo;
+        }
+        parent[nodeNo] = find(parent[nodeNo]);
+        return parent[nodeNo];
     }
 }
