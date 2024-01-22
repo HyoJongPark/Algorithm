00e886c0628fa0c9ec886b8c5c28a8176090d632
박효종 <phj2784@naver.com>
2024-01-22 09:59:30 +0900

    [Gold V] Title: A와 B, Time: 164 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Main {
+    static String S, T;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        S = br.readLine();
+        T = br.readLine();
+
+        if (bfs()) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+    private static boolean bfs() {
+        Queue<String> q = new LinkedList<>();
+        HashSet<String> check = new HashSet<>();
+
+        q.offer(T);
+        check.add(T);
+        while (!q.isEmpty()) {
+            String current = q.poll();
+            if (current.length() == S.length()) continue;
+
+            String next = current.substring(0, current.length() - 1);
+            if (current.endsWith("B")) {
+                next = makeNextString(next);
+
+                if (checkAndPush(next, check, q)) return true;
+            }
+
+            if (checkAndPush(next, check, q)) return true;
+        }
+
+        return false;
+    }
+
+    private static boolean checkAndPush(String next, HashSet<String> check, Queue<String> q) {
+        if (next.equals(S)) return true;
+
+        if (!check.contains(next)) {
+            check.add(next);
+            q.offer(next);
+        }
+        return false;
+    }
+
+    private static String makeNextString(String str) {
+        char[] d = str.toCharArray();
+        char[] next = new char[d.length];
+        for (int i = 0; i < d.length; i++) {
+            next[i] = d[d.length - i - 1];
+        }
+
+        return new String(next);
+    }
+}
