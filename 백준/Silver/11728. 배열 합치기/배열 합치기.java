4c95a731a03feabbbc94024215a316223ea6899f
박효종 <phj2784@naver.com>
2024-02-08 11:11:21 +0900

    [Silver V] Title: 배열 합치기, Time: 1612 ms, Memory: 312688 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -6,25 +6,27 @@ import java.util.StringTokenizer;
 
 class Main {
     static int N, M;
+    static int[] arrA, arrB;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int[] num1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] num2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        arrA = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        arrB = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int pointerA = 0, pointerB = 0;
         StringBuilder sb = new StringBuilder();
-        int point1 = 0, point2 = 0, index = 0;
-        while (point1 < num1.length || point2 < num2.length) {
-            if (point1 == num1.length) sb.append(num2[point2++]);
-            else if (point2 == num2.length) sb.append(num1[point1++]);
-            else if (num1[point1] <= num2[point2]) sb.append(num1[point1++]);
-            else sb.append(num2[point2++]);
+        while (pointerA < N || pointerB < M) {
+            if (pointerA == N || (pointerB != M && arrA[pointerA] > arrB[pointerB])) {
+                sb.append(arrB[pointerB++]);
+            } else {
+                sb.append(arrA[pointerA++]);
+            }
             sb.append(" ");
         }
-
         System.out.println(sb);
     }
 }
