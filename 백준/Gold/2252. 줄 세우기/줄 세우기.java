32a79c434d86b3d05704958f26b4ffde24e7b351
박효종 <phj2784@naver.com>
2024-03-05 09:10:53 +0900

    [Gold III] Title: 줄 세우기, Time: 512 ms, Memory: 47600 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,53 +1,60 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.*;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main {
     static int N, M;
-    static int[] edgeCount;
-    static List<List<Integer>> tree = new ArrayList<>();
-
+    static int[] inDegree;
+    static List<List<Integer>> sequence = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        edgeCount = new int[N + 1];
+        inDegree = new int[N + 1];
+
         for (int i = 0; i <= N; i++) {
-            tree.add(new ArrayList<>());
+            sequence.add(new ArrayList<>());
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-            tree.get(a).add(b);
-            edgeCount[b]++;
+            int before = Integer.parseInt(st.nextToken());
+            int next = Integer.parseInt(st.nextToken());
+
+            inDegree[next]++;
+            sequence.get(before).add(next);
         }
 
-        topologySort();
+        List<Integer> result = topologicalSort();
+        StringBuilder sb = new StringBuilder();
+        for (int i : result) {
+            sb.append(i).append(" ");
+        }
+        System.out.print(sb);
     }
 
-    private static void topologySort() {
-        StringBuilder sb = new StringBuilder();
+    private static List<Integer> topologicalSort() {
         Queue<Integer> q = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
         for (int i = 1; i <= N; i++) {
-            if (edgeCount[i] == 0) {
-                q.offer(i);
-            }
+            if (inDegree[i] == 0) q.offer(i);
         }
 
         while (!q.isEmpty()) {
-            int student = q.poll();
-            sb.append(student).append(" ");
+            Integer current = q.poll();
 
-            for (Integer nextStudent : tree.get(student)) {
-                edgeCount[nextStudent]--;
-                if (edgeCount[nextStudent] == 0) {
-                    q.offer(nextStudent);
-                }
+            result.add(current);
+            for (int next : sequence.get(current)) {
+                if (--inDegree[next] == 0) q.offer(next);
             }
         }
-        System.out.println(sb);
+        return result;
     }
 }
