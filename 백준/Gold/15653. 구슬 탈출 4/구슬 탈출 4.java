c97463b21dc4594a1e7b278773adf7b3b294ab90
박효종 <phj2784@naver.com>
2024-03-20 09:18:10 +0900

    [Gold I] Title: 구슬 탈출 4, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/15653. 구슬 탈출 4/구슬 탈출 4.java
+++ 백준/Gold/15653. 구슬 탈출 4/구슬 탈출 4.java
@@ -5,114 +5,103 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-
+class Main {
     static int N, M;
-    static Point redBall, blueBall;
     static char[][] board;
-    static boolean[][][][] check;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         board = new char[N][M];
-        check = new boolean[N][M][N][M];
+
+        int[] redPos = new int[2];
+        int[] bluePos = new int[2];
         for (int i = 0; i < N; i++) {
             board[i] = br.readLine().toCharArray();
+
             for (int j = 0; j < M; j++) {
-                if (board[i][j] == 'R') {
-                    redBall = new Point(i, j, 0);
-                } else if (board[i][j] == 'B') {
-                    blueBall = new Point(i, j, 0);
-                }
+                if (board[i][j] == 'B') bluePos = new int[]{i, j};
+                else if (board[i][j] == 'R') redPos = new int[]{i, j, 0};
             }
         }
 
-        System.out.println(BFS());
+        int result = bfs(redPos, bluePos);
+        System.out.println(result);
     }
 
-    private static int BFS() {
-        Queue<Point[]> q = new LinkedList<>();
-        q.offer(new Point[]{redBall, blueBall});
-        check[redBall.x][redBall.y][blueBall.x][blueBall.y] = true;
+    private static int bfs(int[] redPos, int[] bluePos) {
+        Queue<int[][]> q = new LinkedList<>();
+        boolean[][][][] check = new boolean[N][M][N][M];
 
+        q.offer(new int[][]{redPos, bluePos});
+        check[redPos[0]][redPos[1]][bluePos[0]][bluePos[1]] = true;
         while (!q.isEmpty()) {
-            Point[] balls = q.poll();
-            Point currentRed = balls[0], currentBlue = balls[1];
+            int[][] balls = q.poll();
+            int[] currentRed = balls[0];
+            int[] currentBlue = balls[1];
 
-            for (int i = 0; i < 4; i++) {
-                //blue 공 이동
-                boolean bluePathHall = false;
-                Point nextBlue = new Point(currentBlue.x, currentBlue.y, currentBlue.count + 1);
-                while (isValid(nextBlue.x + dx[i], nextBlue.y+ dy[i])) {
-                    nextBlue.x += dx[i];
-                    nextBlue.y += dy[i];
-                    if (board[nextBlue.x][nextBlue.y] == 'O') {
-                        bluePathHall = true;
+            for (int i = 0; i < d.length; i++) {
+                //moveBlue
+                int[] nextBlue = new int[]{currentBlue[0], currentBlue[1]};
+                boolean blueIntoHole = false;
+
+                while (isValid(nextBlue[0]  + d[i][0], nextBlue[1] + d[i][1])) {
+                    nextBlue[0] += d[i][0];
+                    nextBlue[1] += d[i][1];
+
+                    if (board[nextBlue[0]][nextBlue[1]] == 'O') {
+                        blueIntoHole = true;
                         break;
                     }
                 }
-                if (bluePathHall) continue;
+                if (blueIntoHole) continue;
 
-                //red 공 이동
-                Point nextRed = new Point(currentRed.x, currentRed.y, currentRed.count + 1);
-                while (isValid(nextRed.x + dx[i], nextRed.y + dy[i])) {
-                    nextRed.x += dx[i];
-                    nextRed.y += dy[i];
-                    if (board[nextRed.x][nextRed.y] == 'O') return currentRed.count + 1;
+                //move red
+                int[] nextRed = new int[]{currentRed[0], currentRed[1], currentRed[2] + 1};
+
+                while (isValid(nextRed[0]  + d[i][0], nextRed[1] + d[i][1])) {
+                    nextRed[0] += d[i][0];
+                    nextRed[1] += d[i][1];
+
+                    if (board[nextRed[0]][nextRed[1]] == 'O') return nextRed[2];
                 }
 
-                //두 공의 위치가 같다면 위치 재조정
-                if (nextRed.equals(nextBlue)) {
-                    if (i == 0) {
-                        if (currentRed.x > currentBlue.x) nextRed.x++;
-                        else nextBlue.x++;
-                    } else if (i == 1) {
-                        if (currentRed.x < currentBlue.x) nextRed.x--;
-                        else nextBlue.x--;
-                    } else if (i == 2) {
-                        if (currentRed.y > currentBlue.y) nextRed.y++;
-                        else nextBlue.y++;
-                    } else {
-                        if (currentRed.y < currentBlue.y) nextRed.y--;
-                        else nextBlue.y--;
-                    }
+                //위치 재조정
+                if (nextBlue[0] == nextRed[0] && nextBlue[1] == nextRed[1]) {
+                    rePosition(currentBlue, currentRed, nextBlue, nextRed, i);
                 }
 
-                //이미 방문한 장소가 아니면, 방문체크 후 큐에 삽입
-                if (!check[nextRed.x][nextRed.y][nextBlue.x][nextBlue.y]) {
-                    q.offer(new Point[]{nextRed, nextBlue});
-                    check[nextRed.x][nextRed.y][nextBlue.x][nextBlue.y] = true;
+                if (!check[nextRed[0]][nextRed[1]][nextBlue[0]][nextBlue[1]]) {
+                    q.offer(new int[][]{nextRed, nextBlue});
+                    check[nextRed[0]][nextRed[1]][nextBlue[0]][nextBlue[1]] = true;
                 }
             }
         }
+        return - 1;
+    }
 
-        return -1;
+    //d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static void rePosition(int[] currentBlue, int[] currentRed, int[] nextBlue, int[] nextRed, int direction) {
+        if (direction == 0) {
+            if (currentBlue[1] > currentRed[1]) nextRed[1]--;
+            else nextBlue[1]--;
+        } else if (direction == 1) {
+            if (currentBlue[1] < currentRed[1]) nextRed[1]++;
+            else nextBlue[1]++;
+        } else if (direction == 2) {
+            if (currentBlue[0] > currentRed[0]) nextRed[0]--;
+            else nextBlue[0]--;
+        } else {
+            if (currentBlue[0] < currentRed[0]) nextRed[0]++;
+            else nextBlue[0]++;
+        }
     }
 
     private static boolean isValid(int nextX, int nextY) {
         return 0 <= nextX && nextX < N && 0 <= nextY && nextY < M
                 && board[nextX][nextY] != '#';
-    }
-
-    static class Point {
-        int x, y;
-        int count;
-
-        public Point(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return x == ((Point) obj).x && y == ((Point) obj).y;
-        }
     }
 }
