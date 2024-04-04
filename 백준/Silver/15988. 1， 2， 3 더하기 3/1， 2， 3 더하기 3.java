02e114356673f1d7afd482c5ed8849a4d1535d3f
박효종 <phj2784@naver.com>
2024-04-04 13:03:39 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 156 ms, Memory: 18256 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -2,26 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
+class Main {
 
-    static int T;
-    static long[] dp = new long[1_000_001];
+    static int T, N, MOD_NUM = 1_000_000_009;
+    static int[] dp = new int[1_000_001];
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
 
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-        for (int i = 4; i < 1_000_001; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009;
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = (((dp[i - 1] + dp[i - 2]) % MOD_NUM) + dp[i - 3]) % MOD_NUM;
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < T; i++){
+        while (T-- > 0) {
             sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
