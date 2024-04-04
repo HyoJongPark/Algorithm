d924d53c6df431fc09622fe5c28d5a84507a01b7
박효종 <phj2784@naver.com>
2024-04-04 11:56:26 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    static int N;
+    static long[] dp = new long[91];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[N]);
+    }
+}
