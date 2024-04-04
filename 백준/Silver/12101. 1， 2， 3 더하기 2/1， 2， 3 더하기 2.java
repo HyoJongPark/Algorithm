9293ec796896b13957770f7cbc3fc40ae689eb8f
박효종 <phj2784@naver.com>
2024-04-04 12:58:54 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 160 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -1,44 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int N, K, cnt = 0;
-    static int[] dp = new int[12];
+class Main {
 
-    public static void main(String[] args) throws IOException{
+    static int N, K, count = 0;
+    static int[] dp = new int[11];
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=4;
-        DFS(0,0);
-        
+        dfs(0, 0);
         System.out.println(-1);
     }
-    public static void DFS(int num, int level) {
-        if(num > N) {
-            return;
-        }
-        if(num == N) {
-            if(++cnt == K) {
-                StringBuilder sb = new StringBuilder();
-                for(int i = 0; i < level - 1; i++) {
-                    sb.append(dp[i]).append("+");
-                }
-                sb.append(dp[level - 1]);
-                System.out.println(sb);
-                System.exit(0);
+
+    private static void dfs(int depth, int num) {
+        if (num > N) return;
+        if (num == N && ++ count == K) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < depth; i++) {
+                sb.append(dp[i]).append("+");
             }
-            return ;
+            sb.deleteCharAt(sb.length() - 1);
+            System.out.println(sb);
+            System.exit(0);
         }
 
-        for(int i = 1; i <= 3; i++) {
-            dp[level] = i;
-            DFS(num + i, level + 1);
+        for (int i = 1; i <= 3; i++) {
+            dp[depth] = i;
+            dfs(depth + 1, num + i);
         }
     }
 }
