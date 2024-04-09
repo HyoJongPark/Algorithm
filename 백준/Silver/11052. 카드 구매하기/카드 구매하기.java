970e432804efc6fbedf0d48500e26dac5f6fa1f8
박효종 <phj2784@naver.com>
2024-04-09 14:24:35 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+
+    static int N;
+    static int[] cost, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        cost = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + cost[j - 1]);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
