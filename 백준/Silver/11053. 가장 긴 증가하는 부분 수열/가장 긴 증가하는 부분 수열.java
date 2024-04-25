5cd98565edecac591db6f536494013fc929de036
박효종 <phj2784@naver.com>
2024-04-26 08:25:30 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 152 ms, Memory: 14732 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -3,30 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-import static java.lang.Math.max;
-
 class Main {
-    static int N, MAX;
-    static int[] nums, dp;
+
+    static int N;
+    static int[] arr, dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        dp = new int[nums.length];
-        dp[0] = 1;
-        MAX = dp[0];
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        dp = new int[N];
 
-        for (int i = 1; i < dp.length; i++) {
+        dp[0] = 1;
+        int result = 1;
+        for (int i = 1; i < N; i++) {
             int max = 0;
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[j] < nums[i] && dp[j] > max) max = dp[j];
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    max = Math.max(max, dp[j]);
+                }
             }
 
             dp[i] = max + 1;
-            MAX = max(MAX, dp[i]);
+            result = Math.max(result, dp[i]);
         }
-
-        System.out.println(MAX);
+        System.out.println(result);
     }
 }
