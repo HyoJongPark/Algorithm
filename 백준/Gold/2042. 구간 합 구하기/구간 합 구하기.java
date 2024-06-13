a00aa2d4f73e78a3fe4c34aabce29fc44283ab9c
박효종 <phj2784@naver.com>
2024-06-13 09:44:05 +0900

    [Gold I] Title: 구간 합 구하기, Time: 564 ms, Memory: 93448 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -4,8 +4,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
+
     static int N, M, K, PIV = 1 << 20;
-    static long[] tree;
+    static long[] tree = new long[2 * PIV];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +14,7 @@ class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        tree = new long[2 * PIV];
+
         for (int i = 0; i < N; i++) {
             tree[PIV + i] = Long.parseLong(br.readLine());
         }
@@ -22,52 +23,57 @@ class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M + K; i++) {
             st = new StringTokenizer(br.readLine());
+
             if (st.nextToken().equals("1")) {
                 change(Integer.parseInt(st.nextToken()), Long.parseLong(st.nextToken()));
             } else {
-                sb.append(print(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))).append("\n");
+                long sum = print(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+                sb.append(sum).append("\n");
             }
         }
-
-        System.out.println(sb);
-    }
-
-    private static void makeTree() {
-        int start = PIV / 2, end = PIV;
-        while (start != 0) {
-            for (int i = start; i < end; i++) {
-                tree[i] = tree[i * 2] + tree[i * 2 + 1];
-            }
-            end /= 2;
-            start /= 2;
-        }
-    }
-
-    private static void change(int position, long nextValue) {
-        position += PIV - 1;
-        long difference = nextValue - tree[position];
-        while (position > 0) {
-            tree[position] += difference;
-            position /= 2;
-        }
+        System.out.print(sb);
     }
 
     private static long print(int start, int end) {
+        long sum = 0;
         start += PIV - 1;
         end += PIV - 1;
-        long sum = 0;
+
         while (start <= end) {
             if (start % 2 == 1) {
                 sum += tree[start++];
             }
-
             if (end % 2 == 0) {
                 sum += tree[end--];
             }
+
             start /= 2;
             end /= 2;
         }
-
         return sum;
+    }
+
+    private static void change(int index, long value) {
+        index += PIV - 1;
+        long differ = value - tree[index];
+
+        while (index != 0) {
+            tree[index] += differ;
+            index /= 2;
+        }
+    }
+
+    private static void makeTree() {
+        int start = PIV / 2, end = PIV;
+
+        while (start != 0) {
+            for (int i = start; i < end; i++) {
+                tree[i] = tree[i * 2] + tree[i * 2 + 1];
+            }
+
+            start /= 2;
+            end /= 2;
+        }
     }
 }
