f8315dc5b193755c315e3dbe1c1535abbf5c8e4d
박효종 <phj2784@naver.com>
2024-07-15 12:22:01 +0900

    [Silver V] Title: 폴리오미노, Time: 156 ms, Memory: 16124 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -3,17 +3,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        String replaceToA = input.replaceAll("XXXX", "AAAA");
-        String replaceToB = replaceToA.replaceAll("XX", "BB");
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '.') {
+                sb.append(makeDomino(count)).append(".");
+                count = 0;
+                continue;
+            }
 
-        if (replaceToB.contains("X")) {
-            System.out.println(-1);
-        } else {
-            System.out.println(replaceToB);
+            count++;
         }
+
+        if (count != 0) {
+            sb.append(makeDomino(count));
+        }
+        System.out.println(sb);
+    }
+
+    private static String makeDomino(int count) {
+        if (count % 2 != 0) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+
+        StringBuilder str = new StringBuilder();
+
+        while (count > 0) {
+            if (count >= 4) {
+                count -= 4;
+                str.append("AAAA");
+            } else {
+                count -= 2;
+                str.append("BB");
+            }
+        }
+        return str.toString();
     }
 }
