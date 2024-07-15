42f9b5233f7150993a91156e95d11200586a60c6
박효종 <phj2784@naver.com>
2024-07-16 08:41:17 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 280 ms, Memory: 24080 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -4,24 +4,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
+    static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] numbers = new int[N];
-        int[] sum = new int[N];
+        N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        numbers[0] = Integer.parseInt(st.nextToken());
-        sum[0] = numbers[0];
-        for (int i = 1; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-            sum[i] = sum[i - 1] + numbers[i];
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+            sum += nums[i];
         }
 
-        long answer = 0;
-        for (int i = 0; i < N - 1; i++) {
-            answer += (long) numbers[i] * (sum[N - 1] - sum[i]);
+        long psum = 0;
+        for (int i = 0; i < N; i++) {
+            sum -= nums[i];
+            psum += (long) sum * nums[i];
         }
-        System.out.println(answer);
+        System.out.println(psum);
     }
 }
