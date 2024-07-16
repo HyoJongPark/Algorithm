731189378edc3fb9f706dc34c66bf03cd9ce8674
박효종 <phj2784@naver.com>
2024-07-17 08:38:45 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 912 ms, Memory: 124696 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -3,37 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-	static int N, M;
-	static int[][] arr, dp;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N + 1][N + 1];
-		dp = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		while (M-- != 0) {
-			st = new StringTokenizer(br.readLine());
-			int startX = Integer.parseInt(st.nextToken()), startY = Integer.parseInt(st.nextToken());
-			int endX = Integer.parseInt(st.nextToken()), endY = Integer.parseInt(st.nextToken());
+class Main {
+    static int N, M;
+    static int[][] psum;
 
-			int result = dp[endX][endY] - dp[endX][startY - 1] - dp[startX - 1][endY] + dp[startX - 1][startY - 1];	
-			sb.append(result).append("\n");
-		}
-		System.out.print(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        psum = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            psum[i][1] = Integer.parseInt(st.nextToken()) + psum[i - 1][1];
+            for (int j = 2; j <= N; j++) {
+                psum[i][j] = psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(calculatePrefixSum(x1, y1, x2, y2)).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static int calculatePrefixSum(int x1, int y1, int x2, int y2) {
+        return psum[x2][y2] - psum[x1 - 1][y2] - psum[x2][y1 - 1] + psum[x1 - 1][y1 - 1];
+    }
 }
