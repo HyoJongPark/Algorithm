4f8e3eafeacb364f4fd946b8f93120106f00785c
박효종 <phj2784@naver.com>
2024-07-16 09:30:15 +0900

    [Silver II] Title: 출석체크, Time: 272 ms, Memory: 27972 KB -BaekjoonHub

--- 백준/Silver/20438. 출석체크/출석체크.java
+++ 백준/Silver/20438. 출석체크/출석체크.java
@@ -1,17 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 class Main {
-
     static int N, K, Q, M;
-    static boolean[] check;
-    static int[] prefixSum;
-    static Set<Integer> sleep = new HashSet<>();
-
+    static int[] psum;
+    static boolean[] check, sleep;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,41 +15,40 @@ class Main {
         K = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
+        psum = new int[N + 3];
         check = new boolean[N + 3];
-        prefixSum = new int[N + 3];
+        sleep = new boolean[N + 3];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < K; i++) {
-            sleep.add(Integer.parseInt(st.nextToken()));
+            sleep[Integer.parseInt(st.nextToken())] = true;
         }
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < Q; i++) {
-            int current = Integer.parseInt(st.nextToken());
-            if (!sleep.contains(current) && !check[current]) {
-                checkStudent(current);
+            int crr = Integer.parseInt(st.nextToken());
+            if (sleep[crr]) continue;
+
+            for (int next = crr; next < check.length; next += crr) {
+                if (sleep[next]) continue;
+
+                check[next] = true;
             }
         }
 
-        for (int i = 3; i < N + 3; i++) {
-            prefixSum[i] = prefixSum[i - 1] + (check[i] ? 0 : 1);
+        for (int i = 3; i < check.length; i++) {
+            psum[i] = psum[i - 1] + (!check[i] ? 1 : 0);
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            sb.append(prefixSum[E] - prefixSum[S - 1]).append("\n");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            sb.append(psum[e] - psum[s - 1]).append("\n");
         }
         System.out.println(sb);
-    }
-
-    private static void checkStudent(int current) {
-        int next, count = 1;
-        while ((next = current * count++) < N + 3) {
-            if (!sleep.contains(next)) {
-                check[next] = true;
-            }
-        }
     }
 }
