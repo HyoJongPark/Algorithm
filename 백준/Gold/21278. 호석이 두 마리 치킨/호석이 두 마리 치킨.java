3dc06b9f0029d3382274edac7ad00f5b2bd4dd98
박효종 <phj2784@naver.com>
2024-07-25 20:18:14 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 204 ms, Memory: 15588 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
+    private static final int INF = 987654321;
+
     static int N, M;
     static int[][] distance;
-
-    private static final int INF = 987654321;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +17,7 @@ class Main {
         M = Integer.parseInt(st.nextToken());
         distance = new int[N + 1][N + 1];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N + 1; i++) {
             Arrays.fill(distance[i], INF);
             distance[i][i] = 0;
         }
@@ -31,11 +31,11 @@ class Main {
         }
 
         floyd();
-        int[] result = findMinValue();
-        System.out.printf("%d %d %d", result[0], result[1], result[2] * 2);
+        int[] result = findResult();
+        System.out.printf("%d %d %d", result[0], result[1], 2 * result[2]);
     }
 
-    private static int[] findMinValue() {
+    private static int[] findResult() {
         int minCost = Integer.MAX_VALUE, nodeA = 0, nodeB = 0;
 
         for (int i = 1; i <= N; i++) {
@@ -43,9 +43,12 @@ class Main {
                 int sum = 0;
 
                 for (int k = 1; k <= N; k++) {
-                    if (k == i || k == j) continue;
+                    if (k == i || k == j) {
+                        continue;
+                    }
                     sum += Math.min(distance[i][k], distance[j][k]);
                 }
+
                 if (sum < minCost) {
                     minCost = sum;
                     nodeA = i;
@@ -60,9 +63,20 @@ class Main {
         for (int k = 1; k <= N; k++) {
             for (int i = 1; i <= N; i++) {
                 for (int j = 1; j <= N; j++) {
-                    distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                    if (distance[i][k] != INF && distance[k][j] != INF) {
+                        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                    }
                 }
             }
+        }
+    }
+
+    static class Node {
+        int num, cost;
+
+        public Node(int num, int cost) {
+            this.num = num;
+            this.cost = cost;
         }
     }
 }
