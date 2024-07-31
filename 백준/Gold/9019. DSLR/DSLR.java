cdf3b93c48ac0581a659339cbd0fa2f7ed488985
박효종 <phj2784@naver.com>
2024-07-31 10:37:21 +0900

    [Gold IV] Title: DSLR, Time: 3744 ms, Memory: 300320 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,61 +1,77 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
-    static class Number {
-        int num;
-        String command;
 
-        public Number(int num, String command) {
-            this.num = num;
-            this.command = command;
+class Main {
+    static int origin;
+    static int target;
+    static char[] register = {'D', 'S', 'L', 'R'};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            origin = Integer.parseInt(st.nextToken());
+            target = Integer.parseInt(st.nextToken());
+            sb.append(BFS()).append("\n");
         }
+
+        System.out.println(sb);
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    private static String BFS() {
+        boolean[] check = new boolean[10000];
+        Queue<DSLR> q = new LinkedList<>();
+        q.offer(new DSLR(origin, ""));
+        check[origin] = true;
 
-        for (int tc = 0; tc < T; tc++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+        while (!q.isEmpty()) {
+            DSLR dslr = q.poll();
 
-            boolean[] visit = new boolean[10000];
-            Queue<Number> q = new LinkedList<>();
-            q.offer(new Number(A, ""));
-            visit[A] = true;
+            for (int i = 0; i < 4; i++) {
+                DSLR next = useRegister(dslr, register[i]);
 
-            while (!q.isEmpty()) {
-                int now = q.peek().num;
-                String command = q.peek().command;
-                q.poll();
-
-                if (now == B) {
-                    System.out.println(command);
-                    break;
-                }
-
-                int next = (now * 2) % 10000;
-                if (!visit[next]) {
-                    visit[next] = true;
-                    q.offer(new Number(next, command + "D"));
-                }
-                next = now == 0 ? 9999 : now - 1;
-                if (!visit[next]) {
-                    visit[next] = true;
-                    q.offer(new Number(next, command + "S"));
-                }
-
-                next = now / 1000 + (now % 1000) * 10;
-                if (!visit[next]) {
-                    visit[next] = true;
-                    q.offer(new Number(next, command + "L"));
-                }
-                next = now % 10 * 1000 + now / 10;
-                if (!visit[next]) {
-                    visit[next] = true;
-                    q.offer(new Number(next, command + "R"));
+                if (next.current == target) {
+                    return next.command;
+                } else if (!check[next.current]) {
+                    check[next.current] = true;
+                    q.offer(next);
                 }
             }
+        }
+        return "ERROR";
+    }
+
+    private static DSLR useRegister(DSLR dslr, char register) {
+        int next;
+
+        if (register == 'D') {
+            next = (dslr.current * 2 > 9999 ? dslr.current * 2 % 10000 : dslr.current * 2);
+        } else if (register == 'S') {
+            next = (dslr.current == 0 ? 9999 : dslr.current - 1);
+        } else if (register == 'L') {
+            next = (dslr.current / 1000) + (dslr.current % 1000 * 10);
+        } else {
+            next = (dslr.current % 10 * 1000) + (dslr.current / 10);
+        }
+        return new DSLR(next, dslr.command + register);
+    }
+
+    static class DSLR {
+        int current;
+        String command;
+
+        public DSLR(int current, String command) {
+            this.current = current;
+            this.command = command;
         }
     }
 }
