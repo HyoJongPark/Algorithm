44a11de4818bea3ae0e8fc7e203de39f6f4a9d90
박효종 <phj2784@naver.com>
2024-12-05 14:12:23 +0900

    [Gold V] Title: 문자열 게임 2, Time: 396 ms, Memory: 20112 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,47 +1,59 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+class Main {
+    static int T;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
-            String str = br.readLine();
-            int k = Integer.parseInt(br.readLine());
+            String s = br.readLine();
+            int length = Integer.parseInt(br.readLine());
 
-            if (k == 1) {
+            if (length == 1) {
                 sb.append("1 1\n");
                 continue;
             }
 
-            int[] alpha = new int[26];
-            for (int j = 0; j < str.length(); j++) {
-                alpha[str.charAt(j) - 'a']++;
-            }
-
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-            for (int j = 0; j < str.length(); j++) {
-                if (alpha[str.charAt(j) - 'a'] < k) continue;
-
-                int count = 1;
-                for (int l = j + 1; l < str.length(); l++) {
-                    if (str.charAt(j) == str.charAt(l)) count++;
-                    if (count == k) {
-                        min = Math.min(min, l - j + 1);
-                        max = Math.max(max, l - j + 1);
-                        break;
-                    }
-                }
-            }
-            if (min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) {
+            int[] result = findResult(s, length);
+            if (result[0] == Integer.MAX_VALUE || result[1] == Integer.MIN_VALUE) {
                 sb.append("-1\n");
             } else {
-                sb.append(String.format("%d %d\n", min, max));
+                sb.append(result[0]).append(" ").append(result[1]).append("\n");
             }
         }
         System.out.println(sb);
+    }
+
+    private static int[] findResult(String s, int length) {
+        int[] count = new int[27];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] < length) {
+                continue;
+            }
+
+            int currentCount = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    currentCount++;
+                }
+
+                if (currentCount == length) {
+                    min = Math.min(min, j - i + 1);
+                    max = Math.max(max, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return new int[]{min, max};
     }
 }
